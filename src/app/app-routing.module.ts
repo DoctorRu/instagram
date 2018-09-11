ddcd329c39d10783bfb0a6e925cf6e2cd30574d9
doctorru <vjblan@gmail.com>
2018-09-11 17:47:41 -0300

    auth links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import {FollowingComponent} from './following/following.component';
 import {FavoritesComponent} from './favorites/favorites.component';
 import {MyPostsComponent} from './my-posts/my-posts.component';
 import {NgModule} from '@angular/core';
+import {SignUpComponent} from './auth/sign-up/sign-up.component';
+import {LoginComponent} from './auth/login/login.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'allposts', component: AllPostsComponent},
   {path: 'following', component: FollowingComponent},
   {path: 'favorites', component: FavoritesComponent},
-  {path: 'myposts', component: MyPostsComponent}
+  {path: 'myposts', component: MyPostsComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
