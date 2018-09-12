75681b9b50a80e02494b7ab56c8767f5c98ee0e9
DoctorRu <vjblan@gmail.com>
2018-09-12 17:18:49 -0300

    firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AllPostsComponent } from './all-posts/all-posts.component';
-import { FollowingComponent } from './following/following.component';
-import { FavoritesComponent } from './favorites/favorites.component';
-import { MyPostsComponent } from './my-posts/my-posts.component';
-import { SignUpComponent } from './auth/sign-up/sign-up.component';
-import { LoginComponent } from './auth/login/login.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {AllPostsComponent} from './all-posts/all-posts.component';
+import {FollowingComponent} from './following/following.component';
+import {FavoritesComponent} from './favorites/favorites.component';
+import {MyPostsComponent} from './my-posts/my-posts.component';
+import {SignUpComponent} from './auth/sign-up/sign-up.component';
+import {LoginComponent} from './auth/login/login.component';
+import {HomeComponent} from './home/home.component';
 import {AppRoutingModule} from './app-routing.module';
 import {FormsModule} from '@angular/forms';
 import {RouteGuard} from './auth/route-guard';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    AllPostsComponent,
-    FollowingComponent,
-    FavoritesComponent,
-    MyPostsComponent,
-    SignUpComponent,
-    LoginComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [RouteGuard],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        AllPostsComponent,
+        FollowingComponent,
+        FavoritesComponent,
+        MyPostsComponent,
+        SignUpComponent,
+        LoginComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
+    providers: [RouteGuard],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
