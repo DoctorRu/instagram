8ed7b27d058783d18cdc0e77dd74c8f11ddf648f
DoctorRu <vjblan@gmail.com>
2018-09-20 01:34:22 -0300

    messaging

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,4 +1,5 @@
 import {EventEmitter} from '@angular/core';
+import * as firebase from 'firebase';
 
 export class UserService {
 
@@ -10,7 +11,33 @@ export class UserService {
 
     set(userFromDatabase) {
         localStorage.setItem('user', JSON.stringify(userFromDatabase));
-        this.statusChange.emit(userFromDatabase)
+
+        const messaging = firebase.messaging();
+
+        messaging.requestPermission()
+            .then(() => {
+
+                firebase.messaging().getToken()
+                    .then(token => {
+                        console.log('Token received: ', token);
+
+                        messaging.onMessage(payload => {
+                            console.log(payload);
+                        });
+
+                        const updates = {};
+                        updates['/users/' + userFromDatabase.uid + '/messageToken'] = token;
+                        return firebase.database().ref().update(updates);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+        this.statusChange.emit(userFromDatabase);
     }
 
     getProfile() {
